ca436ee83ec52f446727e4a9b11366ea9f1bdf46
unicbm <icccbm@outlook.com>
2026-07-04 20:29:47 +0800

    Prepare v0.3.8 release

--- scripts/package-converter.ps1
+++ scripts/package-converter.ps1
@@ -1,5 +1,5 @@
 param(
-    [string]$Version = "0.3.7",
+    [string]$Version = "0.3.8",
     [string]$OutputRoot = "dist",
     [switch]$SkipBuild
 )
--- scripts/package-release.ps1
+++ scripts/package-release.ps1
@@ -1,5 +1,5 @@
 param(
-    [string]$Version = "0.3.7",
+    [string]$Version = "0.3.8",
     [string]$Configuration = "Release",
     [string]$OutputRoot = "dist",
     [string]$DotnetPath = "",
